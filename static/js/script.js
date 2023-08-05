92fd270173add11664df4f3b406f6b62a2eaf2a3
Gabriel Michaud <michaud.gabriel.3@courrier.uqam.ca>
2023-08-05 16:02:44 -0400

    lol hihi

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,9 @@
 /**
+ * Cette methode permet de verifier qu'un champ ne contient aucune virgule.
  *
- *
- *
+ * @param valeur la valeur du champ
+ * @param le id ou il faut ecrire les messages d'erreurs
+ * @return true si il n'y a aucune virgule dans la valeur du champ, sinon false
  */
 function verifierVirgules(valeur, champ_invalide) {
     let i = 0;
@@ -16,14 +18,18 @@ function verifierVirgules(valeur, champ_invalide) {
     return valide;
 }
 /**
+ * Cette methode permet de verifier que le nom entre est valide.
  *
- *
+ * @return true si le nom est valide, sinon false
  */
 function validerNom() {
     let valide = true;
     let virgule = true;
     let nom = document.getElementById("nom");
-    if (nom.length < 3 || nom.length > 20) {
+    if (nom.length = 0 ) {
+        document.getElementById("nom_invalide").innerHTML = "Le champ est obligatoire";
+	valide = false;
+    } else if (nom.length < 3 || nom.length > 20) {
         document.getElementById("nom_invalide").innerHTML = "Le nom doit être composé de 3 à 20 lettre";
 	valide = false;
     } else {
@@ -32,7 +38,17 @@ function validerNom() {
     return (valide && virgule);
 }
 /**
- * Cette methode permet de valider les champs : espece, race, description, adresse et ville
+ *
+ *
+ */
+function validerAge(valide) {
+    let virgule = true;
+    let age = document.getElementById("age");
+    if (age.leng
+}
+/**
+ * Cette methode permet de valider qu'un champ est rempli, ne depasse pas un certain nombre de 
+ * characteres et ne contient aucune virgules.
  *
  * @param champ Le nom du champ que l'on souhaite valider de type chaine de characteres
  * @param max le nombre maximal de characteres acceptes pour le champ
@@ -57,8 +73,7 @@ function validerChamp(champ, max, valide) {
  * Le code a ete pris a lien ci-dessous :
  * https://stackoverflow.com/a/920456
  *
- * @param email le email a valider
- * @return true si l'adresse courriel correspond a l'expression reguliere, sinon faux
+ * @return true si l'adresse courriel correspond a l'expression reguliere, sinon false
  */
 function verifierFormatCourriel() {
     let email = document.getElementById("email");
@@ -70,18 +85,24 @@ function verifierFormatCourriel() {
     return valide;
 }
 /**
+ * Cette methode permet de valider l'email entre
  *
- *
+ * @param valide la validite du formulaire pour l'instant
+ * @return un booleen representant la validite du formulaire apres la validation du champ present
  */
 function validerEmail(valide) {
+    let format = true;
     valide = validerChamp("email", 80, valide);
-    let format = verifierFormatCourriel();
+    if (valide) {
+        format = verifierFormatCourriel();
+    }
     return (valide && format);
 }
 /**
+ * Cette methode permet de valider le formulaire entre par l'utilisateur.
+ * Un message d'erreur explicatif est afficher a cote de chaque champ non valide lors de la soumission.
  *
- *
- *
+ * return true si tous les champs sont valides, sinon false
  */
 function validerFormulaire() {
     let valide = validerNom();
